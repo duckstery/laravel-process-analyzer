29ca8944194411a899e4da7f9ff54b93c8dc7bf1
[Lampart] Dao Huu Duc <huu_duc@lampart-vn.com>
2023-11-07 13:11:40 +0700

    Change namespace

--- src/LaravelAnalyzerConfig.php
+++ src/LaravelAnalyzerConfig.php
@@ -1,6 +1,6 @@
 <?php
 
-use Duckster\Analyzer\AnalyzerConfig;
+use Duckstery\Analyzer\AnalyzerConfig;
 
 class LaravelAnalyzerConfig extends AnalyzerConfig
 {
--- src/Middleware/FlushAnalyzer.php
+++ src/Middleware/FlushAnalyzer.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Duckster\Analyzer\Analyzer;
+use Duckstery\Analyzer\Analyzer;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
